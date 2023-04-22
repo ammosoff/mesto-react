f76f96f7ff2b65bf49cb152f73e5341011679d95
Nikolai Ammosoff <ammosoff@mail.ru>
2023-04-22 23:48:47 +0300

    feat: вынести первые компоненты

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+/* import logo from './logo.svg'; */
+/* import './App.css'; */
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="page">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main className="content">
+      <section className="profile content__profile">
+        <div className="profile__info">
+{/*           <img className="profile__avatar" src="#" alt="Аватар"> */}
+          <button className="profile__button-avatar" type="button" aria-label="Редактировать аватар"></button>
+          <div className="profile__inner">
+            <h1 className="profile__name"></h1>
+            <p className="profile__caption"></p>
+            <button className="profile__button-edit" type="button" aria-label="Редактировать профиль"></button>
+          </div>
+        </div>
+        <button className="profile__button-add" type="button" aria-label="Добавить"></button>
+      </section>
+      <section aria-label="Список мест в которых стоит побывать">
+        <ul className="cards">
+  
+          {/* <!-- контейнер для добавления карточек --> */}
+          
+        </ul>
+      </section>
+    </main>
+  )
+}
+
+export default Main;
