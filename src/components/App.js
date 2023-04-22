924811e07aaa4b8b93ee191f3b6b2282cc056da3
Nikolai Ammosoff <ammosoff@mail.ru>
2023-04-23 00:55:06 +0300

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 /* import logo from './logo.svg'; */
 /* import './App.css'; */
-import React from 'react';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copyright">&copy; {new Date().getFullYear()} Mesto Russia</p>
+      <p className="footer__copyright">
+        &copy; {new Date().getFullYear()} Mesto Russia
+      </p>
     </footer>
-  )
+  );
 }
 
 export default Footer;
